e562ae828326c83fe7c2a54879e4f8c8e03816b8
Elen <15el.shev@gmail.com>
2022-06-08 22:38:20 +0300

    Add function&button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const btns = document.querySelectorAll('div>button');
+const container = document.getElementById('container');
+
+const btnHandler = ({target:{dataset:{flex}}, target})=>{
+  container.style.flexDirection = flex
+}
+for (const btn of btns) {
+  btn.addEventListener('click', btnHandler)
+}

9996642b21760be1342be9b3d3c4bc0173841b68
Elen <15el.shev@gmail.com>
2022-06-09 14:52:42 +0300

    Finish stylizated&function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,10 @@
 const btns = document.querySelectorAll('div>button');
 const container = document.getElementById('container');
 
-const btnHandler = ({target:{dataset:{flex}}, target})=>{
-  container.style.flexDirection = flex
+const btnHandler = ({target:{dataset:{flexDrctn}}, target})=>{
+  console.log(target.style);
+  console.log(flexDrctn);
+  container.style.flexDirection = flexDrctn;
 }
 for (const btn of btns) {
   btn.addEventListener('click', btnHandler)
